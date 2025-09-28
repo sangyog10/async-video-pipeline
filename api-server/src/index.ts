977f09db9ea749f0549ba8036a28c17bf3f21482
Sangyog <sangyog.puri111@gmail.com>
2025-09-28 13:47:47 +0545

    Fixed: Docker compose file running

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -12,7 +12,7 @@ app.use("/api/v1/videos",videoRoute)
 
 
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
     console.log(`Server is running in port ${PORT}`)
 })
