117f4486a48476043e3e52321845bbb7deb5660f
Sangyog <sangyog.puri111@gmail.com>
2025-10-26 17:20:09 +0545

    Added proper pipeline for uploading edited video back to aws

--- worker/src/processor.ts
+++ worker/src/processor.ts
@@ -1,10 +1,12 @@
 import { Job } from "bullmq";
 import db from "./db/database.js";
-import { downloadVideoFromAws } from "./config/aws.config.js";
+import { downloadVideoFromAws, uploadFileFromLocal } from "./config/aws.config.js";
 import { handleAudioExtraction } from "./controller/extractAudio.js";
 import { handleVideoResize } from "./controller/resizeVideo.js";
 import { VideoEditType } from './types/video.type.js'
 import { JobStatus } from "./types/video.type.js";
+
+const downloadedFileStoringFolder = './downloads'
 
 
 export const processVideoJob = async (job: Job) => {
@@ -15,35 +17,50 @@ export const processVideoJob = async (job: Job) => {
     await db.query("UPDATE Video SET status = $1 WHERE id = $2", [JobStatus.PROCESSING, videoId]);
 
     //download video for processing
-    const downloadedVideoPath = await downloadVideoFromAws(bucket, key, "./video");
+    const downloadedVideoPath = await downloadVideoFromAws(bucket, key, downloadedFileStoringFolder);
 
-    //run the process as needed acc to the job name
+    const processedBucketName = bucket;
+    const processedKey = `edited-${key}`
+
     switch (job.name as VideoEditType) {
+      // Handling job for extracting audio
       case VideoEditType.EXTRACT_AUDIO:
         const audioPath = await handleAudioExtraction(downloadedVideoPath);
-        console.log("✅ Extracted audio:", audioPath);
+        console.log(" Extracted audio:", audioPath);
+        await uploadFileFromLocal(processedBucketName, processedKey, audioPath)
+        await db.query("UPDATE Video SET status = $1, processed_bucket = $2, processed_object_key = $3 WHERE id = $4", [JobStatus.COMPLETED, processedBucketName, processedKey, videoId]);
+        console.log("Successfully uploaded the edited file to s3")
         break;
 
+      // Handling job for resizing video
       case VideoEditType.RESIZE_VIDEO:
         const { height, width } = job.data;
         const resizedPath = await handleVideoResize(downloadedVideoPath, height, width);
         console.log("Resized video :", resizedPath);
+        await uploadFileFromLocal(processedBucketName, processedKey, resizedPath)
+        await db.query("UPDATE Video SET status = $1, processed_bucket = $2, processed_object_key = $3 WHERE id = $4", [JobStatus.COMPLETED, processedBucketName, processedKey, videoId]);
+        console.log("Successfully uploaded the edited file to s3")
         break;
 
       default:
         console.log("Unspecified job name")
     }
-    //Todo: Update result to mino
-
-    //update status of the job to completed
-    await db.query("UPDATE Video SET status = $1 WHERE id = $2", [JobStatus.COMPLETED, videoId]);
-
   } catch (error) {
-    console.error(error)
-    await db.query("UPDATE Video SET status = $1 WHERE id = $2", [JobStatus.FAILED, videoId]);
-    throw error
-  }finally{
+    console.error("Error. Set the video status to FAILED", error);
+    try {
+      const res = await db.query(
+        "UPDATE Video SET status = $1 WHERE id = $2",
+        [JobStatus.FAILED, videoId]
+      );
+      console.log("DB update result:", res);
+    } catch (dbErr) {
+      console.error("Failed to update status to FAILED:", dbErr);
+    }
+
+    throw error;
+  } finally {
     //cleanup the local disk
+    //delete downloadedVideoPath
   }
 
 };
